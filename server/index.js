c5a04f34de368dcfd4e881d11cd7c397721d457f
Marc Cornellà <marc.cornella@live.com>
2016-10-09 10:06:36 +0200

    Add URL field for each event
    With this commit each event has a proper link directing to the actual
    event, except PushEvents which direct to the repo since there isn't
    any available link to the commits.

--- server/index.js
+++ server/index.js
@@ -116,7 +116,8 @@ function stripData(data){
           'repo_name': data.repo.name,
           'payload_size': data.payload.size,
           'message': data.payload.commits[0].message,
-          'created': data.created_at
+          'created': data.created_at,
+          'url': data.repo.url
         });
         pushEventCounter++;
       }
@@ -130,7 +131,8 @@ function stripData(data){
         'repo_name': data.repo.name,
         'payload_size': 0,
         'message': data.body,
-        'created': data.created_at
+        'created': data.created_at,
+        'url': data.payload.comment.html_url
       });
     }else if(data.type == 'PullRequestEvent'){
       if (data.payload.pull_request.merged) data.payload.action = 'merged';
@@ -143,7 +145,8 @@ function stripData(data){
         'repo_name': data.repo.name,
         'action': data.payload.action,  // opened, reopened, closed, merged
         'message': data.payload.pull_request.title,
-        'created': data.created_at
+        'created': data.created_at,
+        'url': data.payload.pull_request.html_url
       });
     }else if(data.type == 'IssuesEvent'){
       stripedData.push({
@@ -155,7 +158,8 @@ function stripData(data){
         'repo_name': data.repo.name,
         'action': data.payload.action,  // opened, reopened, closed
         'message': data.payload.issue.title,
-        'created': data.created_at
+        'created': data.created_at,
+        'url': data.payload.issue.html_url
       });
     }
   });
